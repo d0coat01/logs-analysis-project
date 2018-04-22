54386da53b3dc0563e0a9c0bfb714bf7868e3d5b
Daniel Coats <d0coat01@louisville.edu>
2018-04-21 21:57:24 -0700

    Now printing results piecemeal instead of after all queries complete. Also added more comments and descriptions.

--- Reports.py
+++ Reports.py
@@ -7,21 +7,29 @@ class Reports:
     def __init__(self):
         self.db = psql.connect("dbname=news")
 
-    def generate_report(self):
+    def print_report(self):
+
         """Create a text-based report."""
-        report = ("\n"
-                  "**********************************\n"
-                  "* Analysis of Newspaper Articles *\n"
-                  "**********************************\n")
-        report += self.get_top_articles()
-        report += self.get_top_authors()
-        report += self.get_days_with_errors()
-        report += ("**********************************\n"
-                   "*         End of Analysis        *\n"
-                   "**********************************\n")
-        return report
+        # Used (\n) for line separations instead of triple quotes for easy-to-read code.
+        print(("\n"
+               "**********************************\n"
+               "* Analysis of Newspaper Articles *\n"
+               "**********************************\n"))
+        # Printed each function separately so you wouldn't have to wait for all queries to finish before seeing result.
+        print(self.get_top_articles())
+        print(self.get_top_authors())
+        print(self.get_days_with_errors())
+        print("**********************************\n"
+              "*         End of Analysis        *\n"
+              "**********************************\n")
 
     def get_top_articles(self):
+
+        """Get the top 3 articles based on their occurrences in article_log"""
+        # View alert: Used a view called article_log.
+        # article_log is every log whose path contains "/articles/".
+        # The path in article_log is changed to reflect an article slug when possible.
+        # We can join on article_log and article now to get the top 3 articles.
         cur = self.db.cursor()
         query = ("select a.title, count(l.article_slug) as views\n"
                  "from articles a\n"
@@ -40,6 +48,9 @@ class Reports:
         return report
 
     def get_top_authors(self):
+
+        """Get the top authors based on their occurrences in article_log"""
+        # This is similar to top articles except we use the authors table for grouping and selection.
         cur = self.db.cursor()
         query = ("select authors.name, count(article_log.article_slug) as views from authors\n"
                  "left join articles on authors.id = articles.author\n"
@@ -57,6 +68,10 @@ class Reports:
         return report
 
     def get_days_with_errors(self):
+
+        """Get all days that have more than 1% of their requests end in errors."""
+        # Created two views here. One with the total # of requests for each day.
+        # The other with the number of errors in each day.
         cur = self.db.cursor()
         query = ("select to_char( e.date, 'Mon DD, YYYY') as date\n"
                  ",round((e.error_count::decimal/r.request_count::decimal) * 100, 1) as percent_errors\n"
@@ -79,5 +94,5 @@ class Reports:
 
 
 reports = Reports()
-print(reports.generate_report())
+reports.print_report()
 reports.close()
